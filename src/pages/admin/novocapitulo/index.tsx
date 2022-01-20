c71144b1a4e273cd10950503a97ecfb7f8b79cb3
Edson Silva Ramos <edsonsilvaramos@gmail.com>
2022-01-20 20:20:01 -0300

    Correção do SecaoHeadBar

--- src/pages/admin/novocapitulo/index.tsx
+++ src/pages/admin/novocapitulo/index.tsx
@@ -11,12 +11,15 @@ import NavPaginas from "components/NavPaginas";
 import AddBoxIcon from "@material-ui/icons/AddBox";
 
 interface Values {
-  idObra: string;
-  volumeObra: string;
-  numeroCapitulo: string;
-  nomeCapitulo: string;
-  conteudo: string;
-  tipoObra: string;
+    idObra: string;
+    descricaoObra: string;
+    volumeObra: string;
+    numeroCapitulo: string;
+    nomeCapitulo: string;
+    conteudo: string;
+    tipoObra: string;
+    slugCapitulo: string;
+    parteCapitulo: string;
 }
 
 const handleSubmit = (valores: Values) => {
@@ -28,11 +31,14 @@ const NovoCapitulo: React.FC = () => {
   
     const initialValues: Values = {
     idObra: "",
+    descricaoObra: "Mushoku Tensei",
     volumeObra: "",
     numeroCapitulo: "",
     nomeCapitulo: "",
     conteudo: "",
     tipoObra: "",
+    slugCapitulo: "light-novel-mushoku-tensei-jobless-reincarnation-volume-01-capitulo-01",
+    parteCapitulo: "",
   };
 
   return (
@@ -49,6 +55,9 @@ const NovoCapitulo: React.FC = () => {
             <Formik initialValues={initialValues} onSubmit={handleSubmit}>
               <Form >
                
+                <label htmlFor="descricaoObra">Obra: </label>
+                <Field className="InputCampoDados" id="descricaoObra" name="descricaoObra" type="text"/>
+
                 <label htmlFor="volumeObra">Volume: </label>
                 <Field component="select" id="volumeObra" name="volumeObra" className="selectTipoObras larguraInputsAuxiiar">
                     <option value="0" selected={true}>Selecione o volume</option>
@@ -61,11 +70,17 @@ const NovoCapitulo: React.FC = () => {
                 <label htmlFor="numeroCapitulo">Número do capítulo: </label>
                 <Field className="InputCampoDados larguraInputsAuxiiar" id="numeroCapitulo" name="numeroCapitulo" type="number" />
 
+                <label htmlFor="parteCapitulo">Parte capítulo: </label>
+                <Field className="InputCampoDados larguraInputsAuxiiar" id="parteCapitulo" name="parteCapitulo" type="text" />
+
                 <label htmlFor="nomeCapitulo">Nome do capítulo: </label>
                 <Field className="InputCampoDados" id="nomeCapitulo" name="nomeCapitulo" type="text" />
                 
                 <label htmlFor="ordemCapitulo">Ordem do capítulo: </label>
                 <Field className="InputCampoDados larguraInputsAuxiiar" id="ordemCapitulo" name="ordemCapitulo" type="number" />
+
+                <label htmlFor="slugCapitulo">Slug capítulo: </label>
+                <Field className="InputCampoDados" id="slugCapitulo" name="slugCapitulo" type="text" />
                 
                 <label htmlFor="conteudo">Conteudo: </label>
                 <Field className="InputCampoDados inputText" id="conteudo" name="conteudo" as="textarea" />
