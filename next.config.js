78ae954c254fb4f85dc31ce0ee64eb6012f09fb9
Edson Silva Ramos <edsonsilvaramos@gmail.com>
2022-04-13 19:44:08 -0300

    Alterações no arquivo next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 const withImages = require('next-images')
 
 module.exports = withImages({
-    esModule: true
+    esModule: true,
+    trailingSlash: true,
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      },
 })
