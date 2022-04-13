bc51537afcecb4b038773970f917d4c5ce99875f
Edson Silva Ramos <edsonsilvaramos@gmail.com>
2022-04-13 19:54:27 -0300

    Anteração no arquivo next.config.js

--- next.config.js
+++ next.config.js
@@ -10,4 +10,9 @@ module.exports = withImages({
         // !! WARN !!
         ignoreBuildErrors: true,
       },
+      eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+      },
 })
