ccd8515d28591dcf69156b821276dbe1a658bfd6
Edson Silva Ramos <edsonsilvaramos@gmail.com>
2022-05-29 06:38:48 -0300

    Teste 1

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 const withImages = require('next-images')
 
 module.exports = withImages({
@@ -15,4 +18,13 @@ module.exports = withImages({
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
       },
+      webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.plugins.push(new CopyPlugin([
+            { from: path.join(__dirname, 'node_modules/tinymce/skins'), to: path.join(__dirname, 'public/assets/libs/tinymce/skins') }, /// Copy to public folder
+        ]));
+        return config
+    },
+    webpackDevMiddleware: config => {
+        return config
+    },
 })
