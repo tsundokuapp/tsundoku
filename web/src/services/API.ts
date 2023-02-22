61ec1677fd31e5f876a2c9d1e46ee4c6e693c108
Edson Silva Ramos <eds.ramos@outlook.com>
2023-02-22 12:43:46 -0300

    Alterada URL base API

--- web/src/services/API.ts
+++ web/src/services/API.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://localhost:5001/api/',
-    //baseURL: 'https://api-tsun-teste-assinatura-01.azurewebsites.net/api/'
+    //baseURL: 'https://localhost:5001/api/',
+    baseURL: 'https://api-tsun-teste-assinatura-01.azurewebsites.net/api/'
 });
 
 export default API;
