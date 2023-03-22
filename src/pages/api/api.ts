243c316c4ecb9c124315c8f8d3424515453c2c7d
Elienai <elienay.soares07@gmail.com>
2023-03-22 09:03:22 -0300

    chore(api): adicionado comentario auxiliar em API

--- src/pages/api/api.ts
+++ src/pages/api/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios'
-
+import axios from 'axios';
+// TODO: Salvar a URL da API em um arquivo de configuração
 const API = axios.create({
   baseURL: 'https://api-tsun-teste-assinatura-01.azurewebsites.net/api/',
-})
+});
 
-export default API
+export default API;
