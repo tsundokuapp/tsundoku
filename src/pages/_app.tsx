84367d032eddc12a36c8a6dad08780c853fddaf9
Elienai <elienay.soares07@gmail.com>
2023-04-03 08:43:16 -0300

    fix(app): corrige erro hydration na navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,19 @@ import { CustomThemeProvider } from '@/hooks/useThemeContext';
 // import { useLocalStorage } from "usehooks-ts";
 import { GlobalStyle } from '@/styles/globals';
 import { Navbar } from '@/components/Heading/Navbar';
+import { useMounted } from '@/hooks/useMounted';
 
 export default function App({ Component, pageProps }: AppProps) {
   // TODO: verificar se o tema vai ser alterado via localStorage ou DB
   // !as alterações para salvar devem ser feitas no hook useThemeContext.tsx
   // !persiste o tema no localStorage, node 16.14 não suporta
   // const [theme] = useLocalStorage("theme", defaultTheme);
+
+  const hasMounted = useMounted();
+  if (!hasMounted) {
+    return null;
+  }
+
   return (
     <>
       <CustomThemeProvider>
