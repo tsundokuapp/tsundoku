976ce6627e57e3a210d4eac4827501217c3b4ea2
Edson Ramos <edsonsilvaramos@gmail.com>
2023-04-26 23:21:09 -0300

    fix(Estrutura_Pastas): Reorganizadas as pastas e rotas da area administrativa

---
+++ src/pages/admin/admin/usuarios/adicionar/index.tsx
@@ -0,0 +1,5 @@
+export default function Adicionar() {
+    return (
+        <><h1>Adicionar Usuários</h1></>
+    );
+  }
---
+++ src/pages/admin/principal/capitulos/index.tsx
@@ -0,0 +1,5 @@
+export default function Capitulos() {
+    return (
+        <><h1>Capítulos</h1></>
+    );
+  }
