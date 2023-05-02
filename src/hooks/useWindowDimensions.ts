e18f0a9608e28b92cc7af8fa43652af3a7dbc0d4
Elienai Soares <elienay.soares07@gmail.com>
2023-05-02 16:35:52 -0300

    feat(app): adiciona condicoes de mobile

--- src/hooks/useWindowDimensions.ts
+++ src/hooks/useWindowDimensions.ts
@@ -1,8 +1,11 @@
 import { useState, useEffect, useCallback } from "react";
+import { SIZES_RAW } from "@/constants/breakingPoints";
 
 type WindowDimensions = {
   width: number;
   height: number;
+  isMobile: boolean;
+  isTablet: boolean;
 };
 
 export function useWindowDimensions(): WindowDimensions {
@@ -27,11 +30,16 @@ export function useWindowDimensions(): WindowDimensions {
 
 function getWindowDimensions(): WindowDimensions {
   if (typeof window === "undefined") {
-    return { width: 0, height: 0 };
+    return { width: 0, height: 0, isMobile: false, isTablet: false };
   }
+
+  const isMobile = window.innerWidth < SIZES_RAW.MOBILE;
+  const isTablet = window.innerWidth < SIZES_RAW.TABLET;
 
   return {
     width: window.innerWidth,
     height: window.innerHeight,
+    isMobile,
+    isTablet,
   };
 }
