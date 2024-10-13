a356eb3332bba44be79828188141ebd6f4cb2f51
Ganim <guilhermeganim@hotmail.com>
2024-10-13 16:09:40 -0300

    feat: update home, novels and comics page

--- src/app/(webapp)/(projects)/comics/page.tsx
+++ src/app/(webapp)/(projects)/comics/page.tsx
@@ -1,18 +1,56 @@
-import Image from 'next/image';
+import { Title } from '@/components/common/Title';
+import { Cover } from '@/components/project/Cover';
 
 export default function Comics() {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center gap-8 text-center">
-      <Image
-        src="/404.png"
-        alt="404"
-        className="overflow-hidden"
-        width={480}
-        height={480}
-      />
-      <div className="flex flex-col gap-2">
-        <h1 className="text-2xl font-bold uppercase">Página em construção</h1>
-        <p>Em breve teremos novidades!</p>
+    <div className="flex flex-col gap-12">
+      <Title title="Comics da Tsun" />
+      <div className="flex flex-row flex-wrap gap-6">
+        <Cover
+          src="/cover-alya.webp"
+          title="Alya às Vezes Esconde seus Sentimentos em Russo"
+          category="Novel"
+        />
+        <Cover
+          src="/cover-shadow.webp"
+          title="Kage no Jitsuryokusha ni Naritakute"
+          category="Mangá"
+        />
+        <Cover
+          src="/cover-seven.webp"
+          title="Kage no Jitsuryokusha ni Naritakute! Master of Garden ~Shichikage Retsuden~"
+          category="Mangá"
+        />
+        <Cover
+          src="/cover-alya.webp"
+          title="Alya às Vezes Esconde seus Sentimentos em Russo"
+          category="Novel"
+        />
+        <Cover
+          src="/cover-shadow.webp"
+          title="Kage no Jitsuryokusha ni Naritakute"
+          category="Mangá"
+        />
+        <Cover
+          src="/cover-seven.webp"
+          title="Kage no Jitsuryokusha ni Naritakute! Master of Garden ~Shichikage Retsuden~"
+          category="Mangá"
+        />
+        <Cover
+          src="/cover-alya.webp"
+          title="Alya às Vezes Esconde seus Sentimentos em Russo"
+          category="Novel"
+        />
+        <Cover
+          src="/cover-shadow.webp"
+          title="Kage no Jitsuryokusha ni Naritakute"
+          category="Mangá"
+        />
+        <Cover
+          src="/cover-seven.webp"
+          title="Kage no Jitsuryokusha ni Naritakute! Master of Garden ~Shichikage Retsuden~"
+          category="Mangá"
+        />
       </div>
     </div>
   );
