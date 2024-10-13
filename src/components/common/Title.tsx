df2f295cb5b2836ea7688ba48bb91ac735715d00
Ganim <guilhermeganim@hotmail.com>
2024-10-13 16:07:43 -0300

    feat: add title component

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -6,7 +6,7 @@ interface TitleProps {
 }
 export function Title({ title, children }: TitleProps) {
   return (
-    <div className="mb-4 flex flex-1 flex-row justify-between">
+    <div className="flex flex-1 flex-row justify-between">
       <div className="flex flex-row gap-2 text-2xl font-black">
         <span className="text-sky-500">/</span>
         <span className="uppercase">{title}</span>
