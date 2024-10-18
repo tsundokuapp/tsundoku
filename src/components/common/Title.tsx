04e68ebfc6b70e0c0e2429204e4284ee67b50656
Ganim <guilhermeganim@hotmail.com>
2024-10-17 22:01:11 -0300

    chore: update 'title' component to be a 'TitleContainer' child

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -1,22 +1,17 @@
-import type { ComponentProps, ReactNode } from 'react';
+import type { ComponentProps } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 interface TitleProps extends ComponentProps<'div'> {
   title: string;
-  children?: ReactNode;
 }
-export function Title({ title, children, className, ...props }: TitleProps) {
+export function Title({ title, className, ...props }: TitleProps) {
   return (
     <div
-      className={twMerge('flex flex-1 flex-row justify-between', className)}
+      className={twMerge('flex flex-row gap-4 text-2xl font-black', className)}
       {...props}
     >
-      <div className="flex flex-row gap-4 text-2xl font-black">
-        <span className="text-sky-500">/</span>
-        <span className="uppercase">{title}</span>
-      </div>
-
-      {children}
+      <span className="text-sky-500">/</span>
+      <span className="uppercase">{title}</span>
     </div>
   );
 }
