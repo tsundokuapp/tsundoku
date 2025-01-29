6f2775973cad00a7afff4e611cb6987491e004b7
Elienai Soares <elienay.soares07@gmail.com>
2025-01-28 22:05:33 -0300

    feat: add carousel to homepage

---
+++ src/components/common/carousel/Carousel.test.tsx
@@ -0,0 +1,119 @@
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import { Carousel } from './Carousel';
+
+describe('Testes para o componente <Carousel />', () => {
+  describe('Testes de renderização', () => {
+    it('Deve renderizar Carousel corretamente', () => {
+      // Given
+      render(
+        <Carousel images={['/img1.jpg', '/img2.jpg']} autoSlide={false} />,
+      );
+
+      // When
+      const firstImage = screen.getByAltText('Slide 0');
+
+      // Then
+      expect(firstImage).toBeInTheDocument();
+    });
+
+    it('Deve exibir a primeira imagem ao iniciar', () => {
+      // Given
+      render(
+        <Carousel images={['/img1.jpg', '/img2.jpg']} autoSlide={false} />,
+      );
+
+      // When
+      const firstImage = screen.getByAltText('Slide 0');
+
+      // Then
+      expect(firstImage).toHaveAttribute(
+        'src',
+        expect.stringMatching(/\/_next\/image\?url=%2Fimg1.jpg&/),
+      );
+    });
+
+    it('Não deve quebrar se a lista de imagens estiver vazia', () => {
+      // Given
+      render(<Carousel images={[]} />);
+
+      // Then
+      expect(screen.queryByRole('img')).not.toBeInTheDocument();
+    });
+  });
+
+  describe('Testes de interação e comportamento', () => {
+    jest.useFakeTimers();
+
+    it('Deve avançar para a próxima imagem ao clicar no botão "próximo"', async () => {
+      // Given
+      render(
+        <Carousel images={['/img1.jpg', '/img2.jpg']} autoSlide={false} />,
+      );
+
+      // When
+      const nextButton = screen.getByTestId('next-button');
+      fireEvent.click(nextButton);
+
+      // Then
+      expect(screen.getByAltText('Slide 1')).toBeInTheDocument();
+    });
+
+    it('Deve voltar para a imagem anterior ao clicar no botão "anterior"', async () => {
+      // Given
+      render(
+        <Carousel images={['/img1.jpg', '/img2.jpg']} autoSlide={false} />,
+      );
+
+      // When
+      const prevButton = screen.getByTestId('previous-button');
+      fireEvent.click(prevButton);
+
+      // Then
+      expect(screen.getByAltText('Slide 1')).toBeInTheDocument(); // Última imagem, pois circula para trás
+    });
+
+    it('Deve avançar automaticamente se autoSlide for true', () => {
+      // Given
+      render(
+        <Carousel
+          images={['/img1.jpg', '/img2.jpg']}
+          autoSlide={true}
+          autoSlideInterval={3000}
+        />,
+      );
+
+      // When
+      jest.advanceTimersByTime(3000);
+
+      // Then
+      expect(screen.getByAltText('Slide 1')).toBeInTheDocument();
+    });
+
+    it('Não deve avançar automaticamente se autoSlide for false', () => {
+      // Given
+      render(
+        <Carousel images={['/img1.jpg', '/img2.jpg']} autoSlide={false} />,
+      );
+
+      // When
+      jest.advanceTimersByTime(3000);
+
+      // Then
+      expect(screen.getByAltText('Slide 0')).toBeInTheDocument();
+    });
+  });
+
+  describe('Teste de snapshot', () => {
+    it('Deve manter o snapshot do Carousel', () => {
+      // Given
+      const { container } = render(
+        <Carousel images={['/img1.jpg', '/img2.jpg']} />,
+      );
+
+      // Then
+      expect(container).toMatchSnapshot();
+    });
+  });
+});
---
+++ src/components/common/carousel/Carousel.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import {
+  ArrowCircleLeft,
+  ArrowCircleRight,
+} from '@phosphor-icons/react/dist/ssr';
+import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
+
+import { cn } from '@/helpers/twUtils';
+
+interface CarouselProps {
+  images: string[];
+  autoSlide?: boolean;
+  autoSlideInterval?: number;
+}
+
+export const Carousel = ({
+  images,
+  autoSlide = true,
+  autoSlideInterval = 3000,
+}: CarouselProps) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (autoSlide) {
+      const slideInterval = setInterval(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+      }, autoSlideInterval);
+      return () => clearInterval(slideInterval);
+    }
+  }, [autoSlide, autoSlideInterval, images.length]);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length,
+    );
+  };
+
+  return (
+    <div className="relative w-full max-w-3xl">
+      <div className="relative h-72 w-full overflow-hidden rounded-md">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={cn(
+              `absolute inset-0 w-full translate-x-full transform transition-transform duration-500`,
+              {
+                'translate-x-0': index === currentIndex,
+              },
+            )}
+          >
+            <Image
+              height={300}
+              width={350}
+              src={image}
+              alt={`Slide ${index}`}
+              className="h-full w-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
+      <button
+        className="absolute left-0 top-1/2 -translate-y-1/2 transform p-2 text-white"
+        onClick={prevSlide}
+        aria-label="previous"
+        data-testid="previous-button"
+      >
+        <ArrowCircleLeft size={24} />
+      </button>
+      <button
+        className="absolute right-0 top-1/2 -translate-y-1/2 transform p-2 text-white"
+        onClick={nextSlide}
+        aria-label="next"
+        data-testid="next-button"
+      >
+        <ArrowCircleRight size={24} />
+      </button>
+    </div>
+  );
+};
