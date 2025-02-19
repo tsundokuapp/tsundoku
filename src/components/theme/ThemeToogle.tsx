30e159548877a0324e9bb7fde387aeae6348b78b
Guilherme <guilhermeganim@hotmail.com>
2025-02-18 22:50:18 -0300

    feat: add theme options

--- src/components/theme/ThemeToogle.tsx
+++ src/components/theme/ThemeToogle.tsx
@@ -1,27 +1,78 @@
 'use client';
-
-import { Moon, Sun } from '@phosphor-icons/react/dist/ssr';
+// Color Checked
+// Components Checked
+import { Check, Moon, Sun } from '@phosphor-icons/react/dist/ssr';
 import { useTheme } from 'next-themes';
-import * as React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+import { EnterAnimation } from '@/animation/EnterAnimation';
+
+const themeOptions = [
+  { label: 'Light', value: 'theme-light' },
+  { label: 'Sepia', value: 'theme-sepia' },
+  { label: 'Dark Blue', value: 'theme-blue' },
+  { label: 'Dark', value: 'theme-dark' },
+];
 
 export function ThemeToggle() {
+  const dropdownRef = useRef<HTMLDivElement>(null);
   const { theme, setTheme } = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
 
-  function handleThemeToggle() {
-    document.documentElement.classList.add('theme-transition');
-    setTheme(theme === 'light' ? 'dark' : 'light');
-    setTimeout(() => {
-      document.documentElement.classList.remove('theme-transition');
-    }, 100); // A mesma duração definida na transição CSS
+  function handleSelect(newTheme: string) {
+    setTheme(newTheme);
+    setIsOpen(false);
   }
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
   return (
-    <button
-      className="flex h-10 w-10 items-center justify-center rounded-full bg-slate-700 text-white hover:bg-gradient-to-b hover:from-slate-700 hover:to-slate-800"
-      onClick={() => handleThemeToggle()}
-    >
-      <Sun size={24} className="hidden dark:block" />
-      <Moon size={24} className="block dark:hidden" />
-    </button>
+    <div ref={dropdownRef} className="relative">
+      <button
+        className="flex h-10 w-10 items-center justify-center rounded-full bg-appButtonBackground text-appButtonIcon hover:bg-gradient-to-b hover:from-appButtonBackground hover:to-appButtonHover"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {theme === 'dark' || theme === 'blue' ? (
+          <>
+            <Sun size={24} className="block dark:hidden" />
+            <Moon size={24} className="hidden dark:block" />
+          </>
+        ) : (
+          <>
+            <Sun size={24} className="hidden dark:block" />
+            <Moon size={24} className="block dark:hidden" />
+          </>
+        )}
+      </button>
+
+      {isOpen && (
+        <EnterAnimation delay={0.3} className="absolute z-10">
+          <ul className="absolute mt-2 w-36 rounded-md bg-appMenuBackground p-1 shadow-md">
+            {themeOptions.map(({ label, value }) => (
+              <li
+                key={value}
+                onClick={() => handleSelect(value)}
+                className="m-1 flex cursor-pointer items-center rounded-lg bg-appMenuBackground px-3 py-2 text-sm text-appMenuText hover:bg-appMenuHover"
+              >
+                <span className="ml-2">{label}</span>
+                {value === theme && <Check className="ml-auto h-5 w-5" />}
+              </li>
+            ))}
+          </ul>
+        </EnterAnimation>
+      )}
+    </div>
   );
 }
