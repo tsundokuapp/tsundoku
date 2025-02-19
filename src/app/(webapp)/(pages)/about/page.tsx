01b899d489c92f9e49d73c3724e523a260c6cb4a
Guilherme <guilhermeganim@hotmail.com>
2025-02-18 22:52:16 -0300

    feat: add team info

--- src/app/(webapp)/(pages)/about/page.tsx
+++ src/app/(webapp)/(pages)/about/page.tsx
@@ -1,7 +1,21 @@
+// Color Checked
+// Components Checked
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/shadcn/accordion';
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from '@/components/shadcn/avatar';
+
 export default function About() {
   return (
-    <div className="flex flex-1 flex-col items-center gap-8">
-      <div className="flex w-[600px] flex-col gap-4">
+    <div className="flex flex-1 flex-col items-center gap-12">
+      <div className="flex w-[800px] flex-col gap-4">
         <h1 className="mb-6 text-2xl font-bold uppercase">
           BEM-VINDO À TSUNDOKU TRADUÇÕES
         </h1>
@@ -31,6 +45,104 @@ export default function About() {
           mantendo um alto nível de qualidade e uma boa frequência, basta
           acessar Formas de Apoio.
         </p>
+      </div>
+      <div className="w-[800px]">
+        <h1 className="mb-6 text-2xl font-bold uppercase">Nossa Equipe</h1>
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="Administradores">
+            <AccordionTrigger>Administradores</AccordionTrigger>
+            <AccordionContent>
+              <div className="flex w-full space-x-4">
+                <Avatar className="h-16 w-16">
+                  <AvatarImage src="https://s4.anilist.co/file/anilistcdn/user/avatar/large/b5600801-DVnlQJR4iJ96.jpg" />
+                  <AvatarFallback>BR</AvatarFallback>
+                </Avatar>
+                <div className="space-y-1">
+                  <h4 className="text-sm font-semibold">Bravo</h4>
+                  <p className="text-sm">
+                    &quot;Pense positivo e deixe o universo fazer seu
+                    trabalho.&quot;
+                  </p>
+                  <div className="items-left flex w-full flex-col pt-2">
+                    <span className="text-muted-foreground text-xs font-bold">
+                      Obras Favoritas:
+                    </span>
+                    <span className="text-muted-foreground text-xs">
+                      Mushoku Tensei, Bruxa Errante e Sua História
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="Moderadores">
+            <AccordionTrigger>Moderadores</AccordionTrigger>
+            <AccordionContent>
+              <div className="items-left flex w-full flex-col gap-10">
+                <div className="flex w-full space-x-4">
+                  <Avatar className="h-16 w-16">
+                    <AvatarImage src="https://s4.anilist.co/file/anilistcdn/user/avatar/large/b5600801-DVnlQJR4iJ96.jpg" />
+                    <AvatarFallback>BR</AvatarFallback>
+                  </Avatar>
+                  <div className="space-y-1">
+                    <h4 className="text-sm font-semibold">Bravo</h4>
+                    <p className="text-sm">
+                      &quot;Pense positivo e deixe o universo fazer seu
+                      trabalho.&quot;
+                    </p>
+                    <div className="items-left flex w-full flex-col pt-2">
+                      <span className="text-muted-foreground text-xs font-bold">
+                        Obras Favoritas:
+                      </span>
+                      <span className="text-muted-foreground text-xs">
+                        Mushoku Tensei, Bruxa Errante e Sua História
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex w-full space-x-4">
+                  <Avatar className="h-16 w-16">
+                    <AvatarImage src="https://s4.anilist.co/file/anilistcdn/user/avatar/large/b5600801-DVnlQJR4iJ96.jpg" />
+                    <AvatarFallback>BR</AvatarFallback>
+                  </Avatar>
+                  <div className="space-y-1">
+                    <h4 className="text-sm font-semibold">Bravo</h4>
+                    <p className="text-sm">
+                      &quot;Pense positivo e deixe o universo fazer seu
+                      trabalho.&quot;
+                    </p>
+                    <div className="items-left flex w-full flex-col pt-2">
+                      <span className="text-muted-foreground text-xs font-bold">
+                        Obras Favoritas:
+                      </span>
+                      <span className="text-muted-foreground text-xs">
+                        Mushoku Tensei, Bruxa Errante e Sua História
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="Editores">
+            <AccordionTrigger>Editores</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="Revisores">
+            <AccordionTrigger>Revisores</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="Tradutores">
+            <AccordionTrigger>Tradutores</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
     </div>
   );
