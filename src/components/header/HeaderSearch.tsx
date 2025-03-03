5a3c7024417b7559bf125fd2a3a2c7078a7a39d9
Guilherme <guilhermeganim@hotmail.com>
2025-03-03 17:05:41 -0300

    feat: add props and params to seatch component

--- src/components/header/HeaderSearch.tsx
+++ src/components/header/HeaderSearch.tsx
@@ -1,17 +1,40 @@
 // Color Checked
 // Components Checked
-export function HeaderSearch({ ...props }) {
+
+import { MagnifyingGlass } from '@phosphor-icons/react/dist/ssr';
+
+import { cn } from '@/helpers/twUtils';
+
+interface HeaderSearchProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  placeholder?: string;
+  icon?: 'Enter' | 'Search';
+}
+
+export function HeaderSearch({
+  placeholder = 'Buscar...',
+  icon = 'Enter',
+  className,
+  ...props
+}: HeaderSearchProps) {
   return (
-    <form className="bg-appSearchBackground flex w-[320px] items-center gap-3 rounded-md px-5 py-3">
+    <form className="flex items-center gap-3 rounded-md bg-appSearchBackground px-5 py-2.5">
       <input
         type="text"
-        placeholder="Buscar..."
-        className="text-appSearchText placeholder:text-appSearchPlaceholder flex-1 bg-transparent text-sm outline-none"
+        placeholder={placeholder}
+        className={cn(
+          'w-[320px] flex-1 bg-transparent text-sm text-appSearchText outline-none placeholder:text-appSearchPlaceholder',
+          className,
+        )}
         {...props}
       />
-      <div className="border-appSearchPlaceholder text-appSearchPlaceholder flex h-5 w-12 items-center justify-center rounded border border-dashed text-xs">
-        ENTER
-      </div>
+      {icon === 'Enter' ? (
+        <div className="flex h-5 w-12 items-center justify-center rounded border border-dashed border-appSearchPlaceholder text-xs text-appSearchPlaceholder">
+          ENTER
+        </div>
+      ) : (
+        <MagnifyingGlass className="h-5 w-5 text-appSearchPlaceholder" />
+      )}
     </form>
   );
 }
