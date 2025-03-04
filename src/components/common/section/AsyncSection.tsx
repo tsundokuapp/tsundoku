dc08caa2d7a35267354c2e1d8bbc4807a777adfb
Elienai Soares <elienay.soares07@gmail.com>
2025-03-04 16:20:19 -0300

    fix: element position when title is longer than 1 line

--- src/components/common/section/AsyncSection.tsx
+++ src/components/common/section/AsyncSection.tsx
@@ -30,7 +30,7 @@ export const AsyncSection = ({
             <Spinner size={24} className="animate-spin" />
           </div>
         ) : (
-          <EnterAnimation className="flex flex-row flex-wrap items-center justify-center gap-6 sm:justify-start">
+          <EnterAnimation className="flex flex-row flex-wrap items-start justify-center gap-6 sm:justify-start">
             {children}
           </EnterAnimation>
         )}
