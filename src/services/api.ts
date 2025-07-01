9f36f051367824e2c105a4236cf2c590c5f172eb
Elienai Soares <elienay.soares07@gmail.com>
2025-07-01 13:06:55 -0300

    set new environment variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,17 @@
 import axios from 'axios';
 
+const selectAPI = () => {
+  if (process.env.NEXT_PUBLIC_IS_PRODUCTION === 'true') {
+    return process.env.NEXT_PUBLIC_PROD_URL_API;
+  }
+
+  if (process.env.NEXT_PUBLIC_DOCKER_UP === 'true') {
+    return process.env.NEXT_PUBLIC_API_URL_DOCKER;
+  }
+
+  return process.env.NEXT_PUBLIC_API_URL;
+};
+
 export const api = axios.create({
-  baseURL: process.env.DOCKER_UP
-    ? process.env.NEXT_PUBLIC_API_URL_DOCKER
-    : process.env.NEXT_PUBLIC_API_URL,
+  baseURL: selectAPI(),
 });
