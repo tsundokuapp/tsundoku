d013183fa240660dbb3137d45b80dc821721dbc0
Elienai Soares <elienay.soares07@gmail.com>
2025-07-21 01:29:04 -0300

    editor: fix ruler markers

--- src/app/editor/[documentId]/ruler.tsx
+++ src/app/editor/[documentId]/ruler.tsx
@@ -104,12 +104,9 @@ export const Ruler = () => {
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
       onMouseLeave={handleMouseUp}
-      className="relative flex h-6 select-none items-center border-b border-gray-300 print:hidden"
+      className="relative mx-auto flex h-6 w-[816px] select-none items-center border-b border-gray-300 print:hidden"
     >
-      <div
-        id="ruler-container"
-        className="relative mx-auto h-full w-full max-w-[816px]"
-      >
+      <div id="ruler-container" className="relative h-full w-full">
         <Marker
           position={leftMargin}
           isLeft={true}
