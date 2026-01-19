1d215c95f1416da22c4b77522d8eec61405e9f10
Elienai Soares <elienay.soares07@gmail.com>
2026-01-18 21:51:10 -0300

    chore: remove unused

--- src/helpers/ConvertSlugToText.ts
+++
@@ -1,9 +0,0 @@
-export const convertSlugToText = (slug: string, capitalize = true) => {
-  // Converte slug para texto normal e capitaliza a primeira letra de todas as palavras
-  return capitalize
-    ? slug
-        .split('-')
-        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(' ')
-    : slug.replace(/-/g, ' ');
-};
